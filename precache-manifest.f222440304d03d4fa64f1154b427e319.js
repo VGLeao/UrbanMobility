036a9627282328588e279173dd67d7f8a7acaa97
VGLeao <victorgabriel93@gmail.com>
2020-05-10 19:09:02 -0300

    Updates

---
+++ precache-manifest.f222440304d03d4fa64f1154b427e319.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ff54f36a4d7e198d2ddd5ee7ae8604",
+    "url": "/UrbanMobility/index.html"
+  },
+  {
+    "revision": "9e0c5ca515df3d2096c4",
+    "url": "/UrbanMobility/static/css/2.4dce4d4b.chunk.css"
+  },
+  {
+    "revision": "32818c6e74687c642db1",
+    "url": "/UrbanMobility/static/css/main.a25159f4.chunk.css"
+  },
+  {
+    "revision": "9e0c5ca515df3d2096c4",
+    "url": "/UrbanMobility/static/js/2.8affb6cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UrbanMobility/static/js/2.8affb6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32818c6e74687c642db1",
+    "url": "/UrbanMobility/static/js/main.d30d67d3.chunk.js"
+  },
+  {
+    "revision": "dacc6532905b91ad46ca",
+    "url": "/UrbanMobility/static/js/runtime-main.875e91cf.js"
+  },
+  {
+    "revision": "52eb9933a157d11d0da291da8324e1a5",
+    "url": "/UrbanMobility/static/media/hero-bg.52eb9933.jpg"
+  }
+]);
